400e6bf1894cd4ace6e2841fb62492e440a0f635
Blake Darrow <71980368+BlakeDarrow@users.noreply.github.com>
2025-08-27 20:26:46 -0500

    Axis line Transform -> GlobalTransform (#33)
    Also cleaned up old axis line rendering

--- crates/bevy_granite_gizmos/src/gizmos/mod.rs
+++ crates/bevy_granite_gizmos/src/gizmos/mod.rs
@@ -95,6 +95,6 @@ pub use rotate::{
     RotateGizmoParent,
 };
 pub use transform::{
-    despawn_transform_gizmo, draw_axis_line, spawn_transform_gizmo, PreviousTransformGizmo,
+    despawn_transform_gizmo, spawn_transform_gizmo, PreviousTransformGizmo,
     TransformGizmo, TransformGizmoParent,
 };
