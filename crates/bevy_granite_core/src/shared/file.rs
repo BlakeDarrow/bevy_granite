0262a3882bed7cfb60d82f48c4198cb4f726224d
Blake Darrow <71980368+BlakeDarrow@users.noreply.github.com>
2025-09-15 13:26:27 -0500

    attempt to clean event import separators

--- crates/bevy_granite_core/src/shared/file.rs
+++ crates/bevy_granite_core/src/shared/file.rs
@@ -1,16 +1,19 @@
 use bevy::asset::io::file::FileAssetReader;
-use std::{borrow::Cow, path::Path};
+use std::{
+    borrow::Cow,
+    path::{Path, PathBuf},
+};
 
-pub fn rel_asset_to_absolute<'a>(rel_string: &'a str) -> Cow<'a, str> {
-    if !Path::new(&rel_string).is_absolute() {
+pub fn rel_asset_to_absolute(rel_string: &str) -> Cow<'static, str> {
+    let abs_path: PathBuf = if !Path::new(rel_string).is_absolute() {
         FileAssetReader::get_base_path()
-            .join(format!("assets/{}", rel_string))
-            .to_string_lossy()
-            .to_string()
-            .into()
+            .join("assets")
+            .join(rel_string)
     } else {
-        rel_string.into()
-    }
+        PathBuf::from(rel_string)
+    };
+
+    abs_path.to_string_lossy().replace('\\', "/").into()
 }
 
 pub fn absolute_asset_to_rel(abs_string: String) -> Cow<'static, str> {
@@ -20,11 +23,7 @@ pub fn absolute_asset_to_rel(abs_string: String) -> Cow<'static, str> {
         let base_assets_path = FileAssetReader::get_base_path().join("assets");
 
         if let Ok(rel_path) = abs_path.strip_prefix(&base_assets_path) {
-            rel_path
-                .to_string_lossy()
-                .to_string()
-                .replace("\\", "/")
-                .into()
+            rel_path.to_string_lossy().replace('\\', "/").into()
         } else {
             abs_string.into()
         }
