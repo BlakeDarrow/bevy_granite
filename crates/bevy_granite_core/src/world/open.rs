b234cb843ec5abb9ec46933d000e1e1384ecfd9b
Blake Darrow <71980368+BlakeDarrow@users.noreply.github.com>
2025-12-30 11:49:10 -0600

    process more than one event for open world

--- crates/bevy_granite_core/src/world/open.rs
+++ crates/bevy_granite_core/src/world/open.rs
@@ -23,9 +23,7 @@ pub fn open_world_reader(
     mut world_open_reader: MessageReader<RequestLoadEvent>,
     mut world_load_success_writer: MessageWriter<WorldLoadSuccessEvent>,
 ) {
-    if let Some(RequestLoadEvent(path, save_settings, translation)) =
-        world_open_reader.read().next()
-    {
+    for RequestLoadEvent(path, save_settings, translation) in world_open_reader.read() {
         let rel = absolute_asset_to_rel(path.to_string()).to_string();
         deserialize_entities(
             &asset_server,
@@ -66,7 +64,7 @@ pub fn open_world_batch_reader(
     mut world_batch_reader: MessageReader<RequestLoadBatchEvent>,
     mut world_load_batch_success_writer: MessageWriter<WorldLoadBatchSuccessEvent>,
 ) {
-    if let Some(RequestLoadBatchEvent(worlds)) = world_batch_reader.read().next() {
+    for RequestLoadBatchEvent(worlds) in world_batch_reader.read() {
         let mut loaded_paths = Vec::new();
 
         for (path, save_settings, translation) in worlds.iter() {
